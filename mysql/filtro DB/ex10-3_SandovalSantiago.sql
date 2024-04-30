1f8cd57b90f1f28e1d167e32c41b285c5170de9e
Sandoval <sandovalbgasantiago@gmail.com>
2024-04-29 19:41:31 -0500

    filtro finished

---
+++ mysql/filtro DB/ex10-3_SandovalSantiago.sql
@@ -0,0 +1,24 @@
+CREATE SCHEMA filtro;
+USE filtro;
+
+CREATE TABLE IF NOT EXISTS user(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS download(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_product INT,
+    id_user INT,
+    fileName VARCHAR(255) NOT NULL,
+    date DATETIME NOT NULL,
+    FOREGIN KEY (id_product) REFERENCES product(id),
+    FOREGIN KEY (id_user) REFERENCES user(id)
+);
